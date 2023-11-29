f3d004e0314b9c92a2fb9b3c9cca6c9cf2c2e89e
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-11-29 11:24:20 +0200

    Updated App.jsx structure.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,16 @@
+import { Container } from '../Container/Container.styled';
 import Header from '../Header/Header';
+import { SearchForm } from '../SearchForm/SearchForm';
 import ToDoList from '../ToDoList/ToDoList';
 
 function App() {
   return (
     <>
       <Header />
-      <ToDoList />
+      <Container>
+        <SearchForm />
+        <ToDoList />
+      </Container>
     </>
   );
 }
