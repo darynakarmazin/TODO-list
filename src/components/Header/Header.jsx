9e27314ab5bca4fc3ae78b14e46ca7341dffb2f7
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-11-29 11:23:18 +0200

    Added Header component with content and styles (used styled-components)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,18 @@
+import { HeaderTag, HeaderWrapper } from './Header.styled';
+import { Container } from '../Container/Container.styled';
+import { FiCheckCircle } from 'react-icons/fi';
+
 function Header() {
-  return <h1>ToDoList</h1>;
+  return (
+    <HeaderTag>
+      <Container>
+        <HeaderWrapper>
+          <FiCheckCircle size="28px" />
+          TODO List
+        </HeaderWrapper>
+      </Container>
+    </HeaderTag>
+  );
 }
 
 export default Header;
---
+++ src/components/Header/Header.styled.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const HeaderTag = styled.header`
+  background-color: #fff;
+  box-shadow: 0px 4px 10px 4px #9e9e9e;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+
+  olor: #212121;
+  font-size: 18px;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-bottom: 0;
+`;
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  justify-content: flex-start;
+  align-items: center;
+`;
